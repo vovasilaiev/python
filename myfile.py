fe215d2e4c2ae6b6b8191530147b8e2d73b4c637
vovasilaiev <vova.silaiev@mail.ru>
2024-04-19 21:42:26 +0300

    second change

--- myfile.py
+++ myfile.py
@@ -4,14 +4,15 @@ import pandas as pd
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from xls2xlsx import XLS2XLSX
 import os
 
 base_url = "https://www.muiv.ru"
 
-# Specific page with the schedules
+# страница с расписанием
 url = base_url + "/studentu/fakultet-it/raspisaniya/"
 
-# Function to download a file
+# Для скачивания файла
 def download_file(download_url, file_name):
     response = requests.get(download_url)
     if response.status_code == 200:
@@ -21,23 +22,29 @@ def download_file(download_url, file_name):
     else:
         print(f"Failed to download {file_name}")
 
-# Send a GET request
+# Отправка Get запроса
 response = requests.get(url)
 if response.status_code == 200:
     # Parse the HTML
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Find the Бакалавриат block
+    # Поиск Бакалавриат block
     bachelor_block = soup.find('div', class_='mb-5')
     h2 = bachelor_block.find('h2')
     if h2 and h2.text.strip() == "Бакалавриат":
         download_items = bachelor_block.find_all('div', class_='download__item')
         for item in download_items:
             link = item.find('a', class_='download__src')
-            if link and 'href' in link.attrs and link['href'].endswith('.xls'):
+            if link and 'href' in link.attrs and (link['href'].endswith('.xlsx') or link['href'].endswith('.xls')):
                 file_url = base_url + link['href']
-                file_name = link.text.strip() + '.xls'  # Ensure the extension is correct
+                # Извлечение имя файла из URL-адреса
+                file_name = file_url.split('/')[-1] 
                 download_file(file_url, file_name)
+                if file_name.endswith('.xls'):
+                    new_file_name = file_name[:-3] + 'xlsx'
+                    x2x = XLS2XLSX(file_name)
+                    x2x.to_xlsx(new_file_name)
+                    os.remove(file_name)
 else:
     print("Failed to retrieve the page")
 
@@ -45,10 +52,11 @@ else:
 def find_schedule_by_teacher_name(teacher_name, file_path, sheet_names):
     today = datetime.today().strftime('%Y-%m-%d')
     schedule = {}
+
     for sheet in sheet_names:
         try:
             data = pd.read_excel(file_path, sheet_name=sheet, header=None)
-        # Остальная часть обработки листа...
+        # Остальная часть обработки листа
         except ValueError:
             print(f"Лист '{sheet}' не найден в файле.")
             continue
@@ -89,7 +97,7 @@ def find_schedule_by_teacher_name(teacher_name, file_path, sheet_names):
 
 # Функция для вызова диалога выбора файла
 def open_file_dialog():
-    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xlsx")])
     file_path_entry.delete(0, tk.END)
     file_path_entry.insert(0, file_path)
 
@@ -104,7 +112,7 @@ def process_and_show_schedule():
         messagebox.showerror("Ошибка", "Пожалуйста, введите имя преподавателя")
         return
 
-    sheet_names = ['1 курс', '2 курс', '3 курс']  # Список листов для поиска
+    sheet_names = ['1 курс', '1 курс ', '2 курс', '2 курс ', '3 курс', '3 курс ']  # Список листов для поиска
     schedule = find_schedule_by_teacher_name(teacher_name, file_path, sheet_names)
     output_text.delete('1.0', tk.END)
     if schedule:
